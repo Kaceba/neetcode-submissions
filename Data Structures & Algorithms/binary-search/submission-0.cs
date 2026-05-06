aed1a27070b0e9c6938ee98298bff0c79249102d
Roberto Ninno <108926341+Kaceba@users.noreply.github.com>
2026-05-06 10:40:30 +0200

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.cs
@@ -0,0 +1,37 @@
+public class Solution {
+    public int Search(int[] nums, int target) {
+
+        //binary search, start from the middle, then compare by doubles
+        //its never said if the range of numbers starts at 0
+        //so first of all we have to check if the target could be contained in the array by checking max and minimum
+
+        //if max number is smaller than target then -1
+        //if min number is bigger than target then -1
+        if(nums[nums.Length - 1] < target || nums[0] > target){
+            return -1;
+        }
+
+        //now we can check if its contained
+        int mid = nums.Length/2;
+        int minInx = 0;
+        int maxInx = nums.Length - 1;
+
+        while(minInx <= maxInx){
+
+            mid = minInx + (maxInx - minInx) / 2;
+            
+            if(nums[mid] == target){
+                return mid;
+            }
+
+            if(target > nums[mid]){
+                minInx = mid + 1;
+            }
+            else if(target < nums[mid]){
+                maxInx = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
